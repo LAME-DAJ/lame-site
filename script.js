0e19ff28b9a8beb6fe257b7d8e8b7df53449b054
LAME-DAJ <dailyswithdaj@gmail.com>
2026-02-22 04:36:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -136,12 +136,26 @@ I think it's time for my mood to switch.`
   topics: ["God morning", "thoughts", "gangstalking", "betrayal"],
   content: `It's crazy I feel like lost at a game I never asked to play. My life feels worse off than it was before it's all started(when ever that is) the only thing that keeps me from feeling bad is knowing I can't miss what I never had. `
 },{ 
-  id: 1771750369397,
+  id: 19,
   title: "Lost my \"religion \" not my mind",
   date: "2026-02-22",
   type: "text",
   topics: ["Thoughts", "gangstalking", "manipulation"],
   content: `Nah but fr the worst part is this religion vs spirituality aspect of it because before I  noticed what was going on I was happy just believing that there was a higher force protecting and guiding me i didn't know whether it was "god","jesus","theuniverse",my ancestors, or recently passed relatives. I would just thank them all. Now I'm realizing how all that was used against me to confused me and lead me wrong.and now I'm having a hard time believing in any of those people or concepts. If you stand for nothing you'll fall for anything.  If you don't believe in  any body you'll follow any one . I feel like that what just happened to me. I noticed it while it was happening but the fact that I couldn't trust anyone force me to just reject them all church,religion, spirituality, ancestors everthing.but see the pattern that i always turn to one of them when I need guidance so that s what it really come down to. I just cant trust humans to deliver the message out of fear of being manipulated. Choosing what to believe in is hard enough without mind games. People say just reading the text for my self i just feel like it's not enough time to read it all and compare . What if I get to the end and realize there is no real answer.  Clearly someone has been playing "god" in my life or at least attempting to. They keep saying I'm the chosen one but I  wanna know ,chosen by who?`
+},{ 
+  id: 20,
+  title: "My mind",
+  date: "2026-02-22",
+  type: "text",
+  topics: [],
+  content: `Okay maybe i did loose it a little bit but I think that's understandable given the circumstances.  Or maybe it just found it in a place I didn't know it was`
+},{ 
+  id: 21,
+  title: "",
+  date: "2026-02-22",
+  type: "text",
+  topics: [],
+  content: `How do I fight for something I cant prove? When the authority that I have to prove it to  is against me too,what I'm supposed to do? What can I Do.but tell  the story people just like to listen and judge then dismiss it. I cant really bring my self to figure it out when I know it's all an illusion and everybody else already knows the truth.so what I'm supposed to do?`
 }
 ];
 
